fe57d4dc33073e1ad17bbe43dada0da3ff907031
kseyko35 <seyfiercan35@hotmail.com>
2020-05-26 10:34:38 +0300

    added ViewModel package

---
+++ MvvmSample/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmSample.ViewModel;
+using Xamarin.Forms;
+
+namespace MvvmSample
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+       
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new AddContactViewModel();
+            mPicker.SelectedIndex = 0;
+
+        }
+    }
+}
